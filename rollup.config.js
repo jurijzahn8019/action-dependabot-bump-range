3719e2552ff099f7ff7100df09111f1758add30f
Jurij Zahn <jurij.zahn@otto.de>
2021-01-26 07:21:10 +0100

    chore(ci): add default workflow

--- rollup.config.js
+++ rollup.config.js
@@ -6,8 +6,8 @@ import json from "@rollup/plugin-json";
 export default {
   input: "./src/index.ts",
   output: [
-    { file: "./dist/index.cjs.js", format: "cjs" },
-    { file: "./dist/index.es6.js", format: "es" },
+    { file: "./dist/index.cjs.js", format: "cjs", exports: "auto" },
+    { file: "./dist/index.es6.js", format: "es", exports: "auto" },
   ],
   plugins: [
     typescript({ tsconfig: "./tsconfig.prod.json" }),
